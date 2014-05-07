fb919bec982d14f45831634f5a80f8431e2dfa76
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-05-07 15:12:52 +0800

    update RDP.bat: delete files and show progress;

--- RDP.bat
+++ RDP.bat
@@ -8,15 +8,33 @@ rem Usage: Create or edit RDP.txt in the same directory or sub-directories
 rem of this BAT file, type your IP address, username and password in order
 rem and separate them by spaces or tabs. Each line represents an RDP
 rem connection. For example:
-rem     12.34.56.78 Administrator 123456
+rem     12.34.56.78:1234 Administrator 123456
 
+setlocal enabledelayedexpansion
+for /f %%a in ('copy /Z "%~f0" nul') do set "CR=%%a"
 set /a count="0"
+set /a countb="0"
+set /a countc="0"
 set k="HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\LocalDevices"
+
+for /f %%a in ('dir /s/b *.rdp 2^>nul') do set /a countc+=1
+for /f "tokens=*" %%z in ('dir /s/b RDP.txt') do (
+for /f "tokens=1-3" %%a in ('findstr /r "^[^#]" "%%z"') do (
+for /f "tokens=1 delims=:" %%h in ("%%a") do (
+set /a countb+=1
+)))
+
+echo Press any key to delete all %countc% RDP files in this folder and
+echo generate %countb% new RDP files with the credentials in RDP.txt.
+echo.
+pause >nul
+del /S *.rdp >nul 2>nul
 
 for /f "tokens=*" %%z in ('dir /s/b RDP.txt') do (
 for /f "tokens=1-3" %%a in ('findstr /r "^[^#]" "%%z"') do (
 for /f "tokens=1 delims=:" %%h in ("%%a") do (
 for /f "tokens=*" %%d in ('RDP.exe %%c') do (
+echo|set /p="Generating %%h.rdp ... "
 (
 rem Required - User credential:
 echo full address:s:%%a
@@ -64,14 +82,14 @@ rem Add host to registry to bypass the certificate warnings
 reg add %k% /v %%h /f /t REG_DWORD /d 76 >nul
 
 set /a count+=1
-)
-)
-)
-)
+set /a completed=count*100/countb
+<nul set /p "=OK ^(!completed!%%^)         !CR!"
+))))
 
+echo.
 echo.
 echo                 OK! %count% RDP connections generated.
-echo              Press Enter or wait 2 seconds to exit
+echo                      Press any key to exit.
 echo.
 
-timeout /t 2 >nul
+pause >nul
